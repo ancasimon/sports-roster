495fa759a9feedc18f68b0a1b027049432f122c6
Anca Simon <ancarsimon@gmail.com>
2020-05-18 21:24:00 -0500

    finished auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
       <div className="Auth">
-        <h1>Auth Component</h1>
+        <button className="btn btn-light" onClick={this.loginClickEvent}>Log In with Google Auth</button>
       </div>
     );
   }
