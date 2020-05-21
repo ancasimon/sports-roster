e89e350681cf220f3bb58c8110414d2b20d3f302
Anca Simon <ancarsimon@gmail.com>
2020-05-20 20:01:35 -0500

    added team desc and photo

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -26,9 +26,11 @@ class Team extends React.Component {
     const buildPlayers = sortedPlayers.map((player) => <Player key={player.id} player={player} />);
 
     return (
-      <div className="Team m-5">
+      <div className="Team m-3">
         <h1 className="title">Your 1986-1987 Steaua (The Star) Bucharest</h1>
         <p className="font-italic">Brought to you by SportsRoster, LLC</p>
+        <img src="https://editorial.uefa.com/resources/0253-0e9c68776703-be743894d14a-1000/famously_the_steaua_team_that_defeated_barcelona_consisted_entirely_of_romanian_players_-_five-goal_forward_victor_piturca_was_the_star_his_highlight_netting_twice_against_anderlecht_in_the_semis.jpeg" className="teamImg" alt="Steaua football team before the game" />
+        <p>"We were Romanian football's golden generation. We won domestic trophies and two European trophies [the European Cup and the 1986 UEFA Super Cup] – the most important ones – and we had continuity at the highest level for five years." That Steaua side claimed five straight titles and four Romanian Cups, and went a record 104 league games unbeaten from 1984 to the December '89 revolution, as well as reaching the 1988 European Cup semis and 1989 final. "We were a team from eastern Europe, with no money, and we achieved so much because we were very good players and formed a very good team," reminisced forward Victor Piturca.</p>
         <div className="d-flex flex-wrap align-items-center">
           {buildPlayers}
         </div>
